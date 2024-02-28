51e04d30ff2bee683c4a089434af5067dc9ce51d
Ravi-aahirwar <raviAahirwar19@gmail.com>
2024-02-28 20:02:38 +0530

    updated

--- client/src/utils/contexts/ProductsContext.jsx
+++ client/src/utils/contexts/ProductsContext.jsx
@@ -12,7 +12,7 @@ const ProductsProvider = ({ children }) => {
     }
 
     const [state, dispatch] = useReducer(reducer, initialState)
-    const API_URL = process.env.NODE_ENV === "production"? "":"http://localhost:5000/api/data"
+    const API_URL = process.env.NODE_ENV === "production"? "https://binks-ecommerce-mern-backend.vercel.app/api/data":"http://localhost:5000/api/data"
     
     const getApiData = async (url) => {
         dispatch({ type: "LOADING" })
