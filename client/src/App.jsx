7c724e157c8220507a4c2299bbc0d4403beee85a
Ravi-aahirwar <raviAahirwar19@gmail.com>
2024-02-29 07:58:51 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Products from './pages/products/Products'
 import Favourite from './pages/favourite/Favourite'
 import Cart from './pages/cart/Cart'
 import Home from './pages/home/Home'
+import Error from './pages/Error'
 export default function App() {
 
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/products' element={<Products />} />
-          <Route exact path='/favourite' element={<Favourite />} />
-          <Route exact path='/cart' element={<Cart />} />
-        </Routes>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/favourite' element={<Favourite />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
   )
 }
